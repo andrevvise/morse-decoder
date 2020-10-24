7520e1eb36c4fdd50c0c82f48024e2971b177831
Andrei Shauko <andrevvise@gmail.com>
2020-10-24 12:47:46 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let finalString = '';
+    let arrayExpr = [];
+
+    for (let i = 0; i < expr.length; i += 10 ) {
+        arrayExpr.push(expr.slice(i, i + 10));
+    }
+    let arrayMorse = [];
+
+    for (let i = 0; i < arrayExpr.length; i++) {
+            arrayMorse[i] = '';
+            if (arrayExpr[i] === `**********`) arrayMorse[i] = ' ';
+        for (let j = 0; j < arrayExpr[i].length; j += 2) {
+            if (arrayExpr[i][j] === '1' & arrayExpr[i][j + 1] === '0') arrayMorse[i] += '.';
+            if (arrayExpr[i][j] === '1' & arrayExpr[i][j + 1] === '1') arrayMorse[i] += '-';
+        }
+    }
+    for (let i = 0; i < arrayMorse.length; i++) {
+        if (arrayMorse[i] === ' ') {
+            finalString += ' ';
+        } else finalString += MORSE_TABLE[arrayMorse[i]];
+    }
+
+    return finalString;
 }
 
 module.exports = {
